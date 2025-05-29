ec19ca85b1a85bcbf8819616a4659372cadc998c
usagi-z <wzajaczkowski91@gmail.com>
2025-05-29 21:23:30 +0200

    copy static

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ from text_to_textnodes import *
 from textnode import *
 
 def main():
-    copy_while_clobbering('static', 'foo')
+    copy_while_clobbering('static', 'public')
 
 def copy_while_clobbering(source_dir, dest_dir):
     if os.path.exists(dest_dir):
         for item in os.listdir(dest_dir):
             to_delete = os.path.join(dest_dir, item)
             print("Removing " + to_delete)
-            # shutil.rmtree(to_delete)
+            if os.path.isfile(to_delete):
+                os.remove(to_delete)
+            else:
+                shutil.rmtree(to_delete)
     else:
         os.mkdir(dest_dir)
     if os.path.exists(source_dir):
@@ -20,7 +23,7 @@ def copy_while_clobbering(source_dir, dest_dir):
             dest = os.path.join(dest_dir, item)
             if os.path.isfile(src):
                 print("Copying file " + src + " to " + dest)
-                # shutil.copy(src, dest)
+                shutil.copy(src, dest)
             elif os.path.isdir(src):
                 if not os.path.exists(dest):
                     os.mkdir(dest)
